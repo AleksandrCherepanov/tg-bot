a98363b5999fdd6b09cfbed19823c31fc7f72346
Aleksandr Cherepanov <aleksandr.s.cherepanov@gmail.com>
2022-08-21 09:35:34 +0500

    add user methods

--- internal/user/user.go
+++ internal/user/user.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"fmt"
 	"sync"
 	"tg-bot/internal/list"
 )
@@ -9,6 +10,7 @@ type User struct {
 	Id          int64
 	Name        string
 	CurrentList *list.TaskList
+	listStorage *list.TaskListStorage
 }
 
 type UserStorage struct {
@@ -24,6 +26,37 @@ func (userStorage *UserStorage) CreateUser(id int64, name string) int64 {
 	user.Id = id
 	user.Name = name
 	user.CurrentList = nil
+	user.listStorage = list.NewTaskListStorage()
 
 	return user.Id
+}
+
+func (userStorage *UserStorage) GetUserById(id int64) (User, error) {
+	userStorage.Mutex.Lock()
+	defer userStorage.Mutex.Unlock()
+
+	user, ok := userStorage.userList[id]
+	if !ok {
+		return User{}, fmt.Errorf("user with id=%d not found", id)
+	}
+
+	return user, nil
+}
+
+func (userStorage *UserStorage) SetCurrentList(userId int64, listId int64) (list.TaskList, error) {
+	userStorage.Mutex.Lock()
+	defer userStorage.Mutex.Unlock()
+
+	user, err := userStorage.GetUserById(userId)
+	if err != nil {
+		return list.TaskList{}, err
+	}
+
+	userList, err := user.listStorage.GetListById(listId)
+	if err != nil {
+		return list.TaskList{}, err
+	}
+
+	user.CurrentList = &userList
+	return userList, nil
 }
